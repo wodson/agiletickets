8f804305bc670e30ba55d5dc405b9dfc41f05ee4
Lucas Cavalcanti <lucasmrtuner@gmail.com>
2010-12-10 17:46:43 -0200

    Colocando preco nas sessoes

--- src/main/java/br/com/caelum/agiletickets/models/Sessao.java
+++ src/main/java/br/com/caelum/agiletickets/models/Sessao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.agiletickets.models;
 
+import java.math.BigDecimal;
 import java.util.Locale;
 
 import javax.persistence.Entity;
@@ -29,6 +30,8 @@ public class Sessao {
 	private Integer totalIngressos;
 
 	private Integer ingressosReservados = 0;
+
+	private BigDecimal preco;
 
 	public Long getId() {
 		return id;
@@ -92,5 +95,13 @@ public class Sessao {
 
 	public boolean podeReservar(Integer numeroDeIngressos) {
 		return getIngressosDisponiveis() > numeroDeIngressos;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
 	}
 }
